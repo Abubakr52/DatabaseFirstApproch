2b77b8d9fa7f87e00188030fffb02fcbc29821db
Abubakr <sabubakr52@gmail.com>
2022-08-01 20:31:52 +0500

    ProductId make primary key

--- DatabaseFirstApproch/Models/Product.cs
+++ DatabaseFirstApproch/Models/Product.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DatabaseFirstApproch.Models
 {
     public partial class Product
     {
+        [Key]
         public int ProductId { get; set; }
         public string? Name { get; set; }
         public int? SalePrice { get; set; }
